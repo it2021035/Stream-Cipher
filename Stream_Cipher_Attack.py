695d57422f20828754cfd6ad961cf643ac17ba4c
Ioannhs Kalogridhs <it2021035@hua.gr>
2025-04-09 18:28:40 +0300

    Finished second scenario with 2 messages

--- Stream_Cipher_Attack.py
+++ Stream_Cipher_Attack.py
@@ -1,5 +1,5 @@
-plainText1 = "12345"
-plainText2 = "33253"
+plainText1 = "53"
+plainText2 = "74"
 
 IV = int("01001011", 2)
 
@@ -7,8 +7,8 @@ def stream_cipher(plainText1, plainText2, IV):
     cipherText1 = []
     cipherText2 = []
 
-    bit_list1 = [format(num, '08b') for num in list(plainText1.encode('ascii'))]
-    bit_list2 = [format(num, '08b') for num in list(plainText2.encode('ascii'))]
+    bit_list1 = [format(num, '08b') for num in list(int(char) for char in plainText1)]
+    bit_list2 = [format(num, '08b') for num in list(int(char) for char in plainText2)]
 
     for i in range(len(bit_list1)):
         xor_result = int(bit_list1[i], 2) ^ IV
@@ -26,13 +26,27 @@ def stream_cipher_attack(cipherText1, cipherText2):
     else :
         xor_result = [int(cipherText1[i], 2) ^ int(cipherText2[i], 2) for i in range(len(cipherText1))]
 
-    charset="0123456789"
-    prediction=[]
-    
-    while prediction != xor_result:
-        print()
+    predictions = []
 
-    return prediction
+    for xor_val in xor_result:
+        possible_pairs = []
+        for d1 in range(10):  # '0' έως '9'
+            ascii_d1 = ord(str(d1))
+            for d2 in range(10):
+                ascii_d2 = ord(str(d2))
+                if ascii_d1 ^ ascii_d2 == xor_val:
+                    pair = tuple(sorted((d1, d2)))  # βάζει το μικρότερο πρώτο
+                    if pair not in possible_pairs:
+                        possible_pairs.append(pair)
+        predictions.append(possible_pairs)
+
+    for i, pairs in enumerate(predictions):
+        p1_candidates = sorted(set(pair[0] for pair in pairs))
+        p2_candidates = sorted(set(pair[1] for pair in pairs))
+        print(f"Digit {i+1}: p1 = {p1_candidates}, p2 = {p2_candidates}")
+
+
+    return predictions
 
 # Encrypt
 cipher1, cipher2 = stream_cipher(plainText1, plainText2, IV)
