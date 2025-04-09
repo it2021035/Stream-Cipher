d2ad9ae12841d75ffab82d82d6685e5468ed94a3
Ioannhs Kalogridhs <it2021035@hua.gr>
2025-04-09 18:52:29 +0300

    Finished second scenario with 3 messages

--- Stream_Cipher_Attack.py
+++ Stream_Cipher_Attack.py
@@ -1,54 +1,51 @@
-plainText1 = "53"
-plainText2 = "74"
+def ascii_digit_pairs_for_xor(xor_val):
+    unique_pairs = set()
+    for d1 in range(10):
+        ascii_d1 = ord(str(d1))
+        for d2 in range(10):
+            ascii_d2 = ord(str(d2))
+            if ascii_d1 ^ ascii_d2 == xor_val:
+                pair = tuple(sorted((d1, d2)))
+                unique_pairs.add(pair)
+    return list(unique_pairs)
 
-IV = int("01001011", 2)
+def analyze_three_ciphertexts(c1, c2, c3):
+    min_len = min(len(c1), len(c2), len(c3))
+    
+    xor12 = [int(c1[i], 2) ^ int(c2[i], 2) for i in range(min_len)]
+    xor13 = [int(c1[i], 2) ^ int(c3[i], 2) for i in range(min_len)]
+    xor23 = [int(c2[i], 2) ^ int(c3[i], 2) for i in range(min_len)]
 
-def stream_cipher(plainText1, plainText2, IV):
-    cipherText1 = []
-    cipherText2 = []
+    print("Predictions per digit position:\n")
 
-    bit_list1 = [format(num, '08b') for num in list(int(char) for char in plainText1)]
-    bit_list2 = [format(num, '08b') for num in list(int(char) for char in plainText2)]
+    for i in range(min_len):
+        pairs12 = ascii_digit_pairs_for_xor(xor12[i])
+        pairs13 = ascii_digit_pairs_for_xor(xor13[i])
+        pairs23 = ascii_digit_pairs_for_xor(xor23[i])
 
-    for i in range(len(bit_list1)):
-        xor_result = int(bit_list1[i], 2) ^ IV
-        cipherText1.append(format(xor_result, '08b'))
+        # Gather candidate digits
+        p1 = sorted(set(a for a, b in pairs12) & set(a for a, b in pairs13))
+        p2 = sorted(set(b for a, b in pairs12) & set(a for a, b in pairs23))
+        p3 = sorted(set(b for a, b in pairs13) & set(b for a, b in pairs23))
 
-    for i in range(len(bit_list2)):
-        xor_result = int(bit_list2[i], 2) ^ IV
-        cipherText2.append(format(xor_result, '08b'))
+        print(f"Digit {i+1}:")
+        print(f"  p1 ∈ {p1}")
+        print(f"  p2 ∈ {p2}")
+        print(f"  p3 ∈ {p3}")
+        print("")
 
-    return cipherText1, cipherText2
-
-def stream_cipher_attack(cipherText1, cipherText2):
-    if len(cipherText1)>len(cipherText2):
-        xor_result = [int(cipherText1[i], 2) ^ int(cipherText2[i], 2) for i in range(len(cipherText2))]
-    else :
-        xor_result = [int(cipherText1[i], 2) ^ int(cipherText2[i], 2) for i in range(len(cipherText1))]
-
-    predictions = []
-
-    for xor_val in xor_result:
-        possible_pairs = []
-        for d1 in range(10):  # '0' έως '9'
-            ascii_d1 = ord(str(d1))
-            for d2 in range(10):
-                ascii_d2 = ord(str(d2))
-                if ascii_d1 ^ ascii_d2 == xor_val:
-                    pair = tuple(sorted((d1, d2)))  # βάζει το μικρότερο πρώτο
-                    if pair not in possible_pairs:
-                        possible_pairs.append(pair)
-        predictions.append(possible_pairs)
-
-    for i, pairs in enumerate(predictions):
-        p1_candidates = sorted(set(pair[0] for pair in pairs))
-        p2_candidates = sorted(set(pair[1] for pair in pairs))
-        print(f"Digit {i+1}: p1 = {p1_candidates}, p2 = {p2_candidates}")
+def stream_cipher(plainText, IV):
+    return [format(ord(ch) ^ IV, '08b') for ch in plainText]
 
 
-    return predictions
+p1 = "123234"
+p2 = "432234"
+p3 = "789345"
 
-# Encrypt
-cipher1, cipher2 = stream_cipher(plainText1, plainText2, IV)
+IV = int("10110101", 2)
 
-print( stream_cipher_attack(cipher1,cipher2))
+c1 = stream_cipher(p1, IV)
+c2 = stream_cipher(p2, IV)
+c3 = stream_cipher(p3, IV)
+
+analyze_three_ciphertexts(c1, c2, c3)
